e5c2b238337e840a4d0d1d9bc21c81d04db9cb08
Daniel Shiposha <shipdan@yandex.ru>
2019-04-24 00:27:07 +0300

    restore tests

--- tests/gridfunction.cpp
+++ tests/gridfunction.cpp
@@ -35,34 +35,34 @@ TEST_CASE("functiom creation", "[metaxxa::GridFunction]")
             f(1.55, 1) = 6.14
         );
 
-        // using F = decltype(func);
+        using F = decltype(func);
 
-        // static_assert(is_same_v<typename F::Result, double>);
-        // static_assert(is_same_v<typename F::template Argument<0>, double>);
-        // static_assert(is_same_v<typename F::template Argument<1>, int>);
+        static_assert(is_same_v<typename F::Result, double>);
+        static_assert(is_same_v<typename F::template Argument<0>, double>);
+        static_assert(is_same_v<typename F::template Argument<1>, int>);
 
-        // REQUIRE(func.nodes_count() == 2);
+        REQUIRE(func.nodes_count() == 2);
     }
 
-    // SECTION("General c-tor std tuple")
-    // {
-    //     std::tuple<double, double, int> v1(3.14, 1.42, 0);
-    //     std::tuple<double, double, int> v2(6.14, 1.55, 1);
+    SECTION("General c-tor std tuple")
+    {
+        std::tuple<double, double, int> v1(3.14, 1.42, 0);
+        std::tuple<double, double, int> v2(6.14, 1.55, 1);
 
-    //     GridFunction func
-    //     (
-    //         v1,
-    //         v2
-    //     );
+        GridFunction func
+        (
+            v1,
+            v2
+        );
 
-    //     using F = decltype(func);
+        using F = decltype(func);
 
-    //     static_assert(is_same_v<typename F::Result, double>);
-    //     static_assert(is_same_v<typename F::template Argument<0>, double>);
-    //     static_assert(is_same_v<typename F::template Argument<1>, int>);
+        static_assert(is_same_v<typename F::Result, double>);
+        static_assert(is_same_v<typename F::template Argument<0>, double>);
+        static_assert(is_same_v<typename F::template Argument<1>, int>);
 
-    //     REQUIRE(func.nodes_count() == 2);
-    // }
+        REQUIRE(func.nodes_count() == 2);
+    }
     
     // TODO: getting node
 }
