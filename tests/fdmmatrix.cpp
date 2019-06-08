77a59fec281afb8969b74028f7260f7455027527
Daniel Shiposha <shipdan@yandex.ru>
2019-06-08 04:25:47 +0300

    Add fdm_matrix test

--- tests/fdmmatrix.cpp
+++ tests/fdmmatrix.cpp
@@ -7,4 +7,14 @@ TEST_CASE("fdm_matrix")
         .c_coeff([](auto x) { return x*10; })
         .b_coeff([](auto x) { return x; })
         .compute();
+
+    REQUIRE(m(0, 0) == 1);
+    REQUIRE(m(1, 1) == 2);
+    REQUIRE(m(2, 2) == 3);
+
+    REQUIRE(m(0, 1) == 10);
+    REQUIRE(m(1, 2) == 20);
+
+    REQUIRE(m(1, 0) == -2);
+    REQUIRE(m(2, 1) == -3);
 }
