0b48d1d163f6e573abf95d32c02be49c740bc197
Daniel Shiposha <shipdan@yandex.ru>
2019-06-18 18:16:26 +0300

    Add SizeND

--- include/size.h
+++ include/size.h
@@ -5,20 +5,159 @@
 
 namespace mafox
 {
-    template <typename T>
-    struct Size2D
+    template <std::size_t N, typename T>
+    struct SizeND
     {
-        T width;
-        T height;
+        T dimension[N];
+
+        template <std::size_t INDEX>
+        auto &get()
+        {
+            static_assert(INDEX < N);
+
+            return dimension[INDEX];
+        }
+
+        template <std::size_t INDEX>
+        const auto &get() const
+        {
+            return const_cast<SizeND*>(this)->template get<INDEX>();
+        }
     };
 
     template <typename T>
-    struct Size3D
+    struct SizeND<1, T>
     {
-        T width;
-        T height;
-        T length;
+        SizeND()
+        : length(dimension[0])
+        {}
+
+        SizeND(const T &l)
+        : SizeND()
+        {
+            dimension[0] = l;
+        }
+
+        template <std::size_t INDEX>
+        auto &get()
+        {
+            static_assert(INDEX < 1);
+
+            return dimension[INDEX];
+        }
+
+        template <std::size_t INDEX>
+        const auto &get() const
+        {
+            return const_cast<SizeND*>(this)->template get<INDEX>();
+        }
+
+        T dimension[1];
+        T &length;
     };
+
+    template <typename T>
+    struct SizeND<2, T>
+    {
+        SizeND()
+        : width(dimension[0]), height(dimension[1])
+        {}
+
+        SizeND(const T &w, const T &h)
+        : SizeND()
+        {
+            dimension[0] = w;
+            dimension[1] = h;
+        }
+
+        template <std::size_t INDEX>
+        auto &get()
+        {
+            static_assert(INDEX < 2);
+
+            return dimension[INDEX];
+        }
+
+        template <std::size_t INDEX>
+        const auto &get() const
+        {
+            return const_cast<SizeND*>(this)->template get<INDEX>();
+        }
+
+        T dimension[2];
+        T &width;
+        T &height;
+    };
+
+    template <typename T>
+    struct SizeND<3, T>
+    {
+        SizeND()
+        : width(dimension[0]), height(dimension[1]), length(dimension[2])
+        {}
+
+        SizeND(const T &w, const T &h, const T &l)
+        : SizeND()
+        {
+            dimension[0] = w;
+            dimension[1] = h;
+            dimension[2] = l;
+        }
+
+        template <std::size_t INDEX>
+        auto &get()
+        {
+            static_assert(INDEX < 3);
+
+            return dimension[INDEX];
+        }
+
+        template <std::size_t INDEX>
+        const auto &get() const
+        {
+            return const_cast<SizeND*>(this)->template get<INDEX>();
+        }
+
+        T dimension[3];
+        T &width;
+        T &height;
+        T &length;
+    };
+
+    template <typename T>
+    using Size1D = SizeND<1, T>;
+
+    template <typename T>
+    using Size2D = SizeND<2, T>;
+
+    template <typename T>
+    using Size3D = SizeND<3, T>;
+}
+
+namespace std
+{
+    template <std::size_t INDEX, std::size_t N, typename T>
+    class tuple_element<INDEX, mafox::SizeND<N, T>>
+    {
+    public:
+        using type = T;
+    };
+
+    template <std::size_t N, typename T>
+    class tuple_size<mafox::SizeND<N, T>> : public integral_constant<size_t, N>
+    {};
+
+    template <std::size_t INDEX, std::size_t N, typename T>
+    auto &get(mafox::SizeND<N, T> &size) noexcept
+    {
+        return size.template get<INDEX>();
+    }
+
+    template <std::size_t INDEX, std::size_t N, typename T>
+    const auto &get(const mafox::SizeND<N, T> &size) noexcept
+    {
+        return std::get<INDEX>(const_cast<mafox::SizeND<N, T> &>(size));
+    }
 }
 
 #endif // MAFOX_SIZE_H
--- tests/size.cpp
+++ tests/size.cpp
@@ -11,6 +11,9 @@ TEST_CASE("Size2D")
     auto [w, h] = size;
     REQUIRE(w == 600.0);
     REQUIRE(h == 800.0);
+
+    REQUIRE(std::get<0>(size) == 600.0);
+    REQUIRE(std::get<1>(size) == 800.0);
 }
 
 TEST_CASE("Size3D")
@@ -26,4 +29,8 @@ TEST_CASE("Size3D")
     REQUIRE(w == 600.0);
     REQUIRE(h == 800.0);
     REQUIRE(l == 100.0);
+
+    REQUIRE(std::get<0>(size) == 600.0);
+    REQUIRE(std::get<1>(size) == 800.0);
+    REQUIRE(std::get<2>(size) == 100.0);
 }
